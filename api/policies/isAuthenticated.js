26984b0903d2c33c7d5ed1d023a8a0e433f28285
flavioh825 <flavioh825@gmail.com>
2016-08-16 22:58:45 -0300

    Auth Funcional

---
+++ api/policies/isAuthenticated.js
@@ -0,0 +1,10 @@
+// api/policies/authenticated.js
+
+module.exports = function(req, res, next) {
+   if (req.isAuthenticated()) {
+        return next();
+    }
+    else{
+        return res.redirect('/login');
+    }
+};
